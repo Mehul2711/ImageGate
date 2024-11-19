e3d84dbca7fe6c69e88425a3d0a9a4699b7272fc
Mehul Kumar <mehul271102@gmail.com>
2024-11-19 10:55:17 +0530

    Update profile.js

--- app/components/homepage/profile.js
+++ app/components/homepage/profile.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState } from "react";
-import { FaUserAlt } from "react-icons/fa"; // Use a Font Awesome user icon for elegance
+import { FaUserAlt } from "react-icons/fa"; 
 
 export default function Profile({ user, handleLogout }) {
   const [dropdownOpen, setDropdownOpen] = useState(false);
@@ -12,19 +12,19 @@ export default function Profile({ user, handleLogout }) {
   return (
     <div className="absolute top-4 right-4 flex items-center space-x-4">
       <div className="relative">
-        {/* Profile Icon */}
+    
         <div
           className="w-14 h-14 bg-gradient-to-r from-blue-600 to-purple-500 rounded-full flex items-center justify-center text-white cursor-pointer shadow-lg hover:shadow-xl transition-shadow duration-300"
           onClick={toggleDropdown}
           style={{
-            boxShadow: "0 4px 15px rgba(0, 0, 255, 0.6)", // Soft glowing effect
+            boxShadow: "0 4px 15px rgba(0, 0, 255, 0.6)", 
           }}
         >
           <FaUserAlt className="text-2xl" />{" "}
-          {/* Using Font Awesome User Icon */}
+        
         </div>
 
-        {/* Dropdown Menu */}
+      
         {dropdownOpen && (
           <div className="absolute right-0 mt-2 w-56 bg-white rounded-lg shadow-lg transition-opacity duration-300 z-50">
             <div className="p-4 text-center">
